643fdb485bf471ca04806853ae6f9bf047f3b573
stepan_vasiliuk <stepupka@gmail.com>
2022-09-06 15:52:58 +0300

    Lesson1 3 task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,10 @@
 function ucFirst(str) {
-  // ваш код...
+  if (!str) {
+    return '';
+  }
+  let newString = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    newString += str[i];
+  }
+  return newString;
 }
