833a71f35834dd1041d258ff62bcbd52686bfd68
stepan_vasiliuk <stepupka@gmail.com>
2022-09-07 12:04:06 +0300

    1lesson 4-th task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  let stringToCheck = str.toUpperCase();
+  let deniedValue1 = '1XBET';
+  let deniedValue2 = 'XXX';
+  if (stringToCheck.includes(deniedValue2) || stringToCheck.includes(deniedValue1)) {
+    return true;
+  } else {
+    return false;
+  }
 }
