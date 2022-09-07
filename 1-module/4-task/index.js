8365359ffe1cb6364a4d4a8aa6aef2fac76ee000
stepan_vasiliuk <stepupka@gmail.com>
2022-09-07 16:27:11 +0300

    4-th task fixing

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -2,9 +2,5 @@ function checkSpam(str) {
   let stringToCheck = str.toUpperCase();
   let deniedValue1 = '1XBET';
   let deniedValue2 = 'XXX';
-  if (stringToCheck.includes(deniedValue2) || stringToCheck.includes(deniedValue1)) {
-    return true;
-  } else {
-    return false;
-  }
+  return (stringToCheck.includes(deniedValue2) || stringToCheck.includes(deniedValue1));
 }
