3667114b8a836cb4cd046991deca3beb457bdc98
stepan_vasiliuk <stepupka@gmail.com>
2022-09-22 17:23:14 +0300

    4-th module 3-rd task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,38 @@
 function highlight(table) {
-  // ваш код...
+  const rowsHTMLCollection = table.rows;
+
+  const statusIndex = getColumnIndex('Status');
+  const ageIndex = getColumnIndex('Age');
+  const genderIndex = getColumnIndex('Gender');
+
+  function getColumnIndex(textParameter) {
+    const cellsHTMLCollection = table.rows[0].cells;
+    for (let i = 0; i < cellsHTMLCollection.length; i++) {
+      if (cellsHTMLCollection[i].textContent === textParameter) {
+        return i;
+      }
+    }
+  }
+
+  for (let i = 1; i < rowsHTMLCollection.length; i++) {
+
+    if (!(rowsHTMLCollection[i].cells[statusIndex].hasAttribute('data-available'))) {
+      rowsHTMLCollection[i].setAttribute('hidden', true);
+    }
+    else if ((rowsHTMLCollection[i].cells[statusIndex].dataset.available) == 'true') {
+      rowsHTMLCollection[i].classList.add('available');
+    }
+    else {
+      rowsHTMLCollection[i].classList.add('unavailable');
+    }
+    if (rowsHTMLCollection[i].cells[genderIndex].textContent === 'm') {
+      rowsHTMLCollection[i].classList.add('male');
+    }
+    if (rowsHTMLCollection[i].cells[genderIndex].textContent === 'f') {
+      rowsHTMLCollection[i].classList.add('female');
+    }
+    if (+rowsHTMLCollection[i].cells[ageIndex].textContent < 18) {
+      rowsHTMLCollection[i].style.textDecoration = 'line-through';
+    }
+  }
 }
