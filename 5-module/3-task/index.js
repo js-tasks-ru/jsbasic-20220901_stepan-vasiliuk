b757163f45c179aae2beb0456e7ccd8aac9e8921
stepan_vasiliuk <stepupka@gmail.com>
2022-09-26 11:52:08 +0300

    5-module 3 task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,46 @@
 function initCarousel() {
-  // ваш код...
+  const rightButton = document.querySelector('.carousel__arrow_right');
+  const leftButton = document.querySelector('.carousel__arrow_left');
+
+  const carouselInner = document.querySelector('.carousel__inner');
+  const slides = carouselInner.querySelectorAll('.carousel__slide');
+
+  let carouselTransform = 0;
+  let clickCounter = 0;
+
+  checkCounter();
+
+
+  rightButton.addEventListener('click', () => {
+    carouselTransform -= getOffSetSize(carouselInner);
+    carouselInner.style.transform = `translateX(${carouselTransform}px)`;
+    clickCounter += 1;
+    checkCounter();
+    console.log(carouselTransform);
+  })
+
+  leftButton.addEventListener('click', () => {
+    carouselTransform += getOffSetSize(carouselInner);
+    carouselInner.style.transform = `translateX(${carouselTransform}px)`;
+    clickCounter -= 1;
+    checkCounter();
+    console.log(carouselTransform);
+  })
+
+  function checkCounter() {
+    if (clickCounter >= slides.length - 1) {
+      rightButton.style.display = 'none';
+      leftButton.style.display = '';
+    }
+    else if (clickCounter == 0) {
+      leftButton.style.display = 'none';
+      rightButton.style.display = '';
+    }
+    else {
+      leftButton.style.display = '';
+      rightButton.style.display = '';
+    }
+  }
+
+  function getOffSetSize(elem) { return elem.offsetWidth };
 }
