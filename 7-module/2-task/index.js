d29950e0f38d00b05e01601cb205aeb58294112e
stepan_vasiliuk <stepupka@gmail.com>
2022-10-07 16:17:55 +0300

    7-th module 2-nd task: rendering

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,55 @@
-import createElement from '../../assets/lib/create-element.js';
+﻿import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.#render();
+  }
+
+  setTitle(title) {
+    this.modalTitle.textContent = title;
+  }
+
+  setBody(element) {
+    this.modalBody.insertAdjacentElement('afterbegin', element);
+  }
+
+  open() {
+    this.container.append(this.elem);
+    document.body.classList.toggle('is-modal-open');
+  }
+
+  #render() {
+    this.elem = createElement(this.#template());
+    this.#initElements();
+  }
+
+  #initElements() {
+    this.modalTitle = this.elem.querySelector('.modal__title');
+    this.modalBody = this.elem.querySelector('.modal__body');
+    this.container = document.body.querySelector('.container');
+  }
+
+  #template() {
+    return `
+  <!--Корневой элемент Modal-->
+  <div class="modal">
+    <!--Прозрачная подложка перекрывающая интерфейс-->
+    <div class="modal__overlay"></div>
+
+    <div class="modal__inner">
+      <div class="modal__header">
+        <!--Кнопка закрытия модального окна-->
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+
+        <h3 class="modal__title">
+        </h3>
+      </div>
+
+      <div class="modal__body">
+      </div>
+    </div>
+  </div>`;
   }
 }
