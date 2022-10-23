47793a6709f5ead1a6d22cc4fe236ed916e8e87e
stepan_vasiliuk <stepupka@gmail.com>
2022-10-23 17:34:07 +0300

    8module 2-nd task

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -1,9 +1,108 @@
-import createElement from '../../assets/lib/create-element.js';
-import ProductCard from '../../6-module/2-task/index.js';
+import createElement from "../../assets/lib/create-element.js";
+import ProductCard from "../../6-module/2-task/index.js";
 
 export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.#render();
+  }
+
+  #render() {
+    this.elem = createElement(this.#mainTemplate());
+    this.#initElements();
+    this.#productsTemplate();
+  }
+
+  #initElements() {
+    this.innerGrid = this.elem.querySelector(".products-grid__inner");
+  }
+
+  #mainTemplate() {
+    return `
+    <div class="products-grid">
+       <div class="products-grid__inner">
+       </div>
+    </div>`;
+  }
+
+  #productsTemplate() {
+    this.products.forEach((product) => {
+      let productCard = new ProductCard(product);
+      this.innerGrid.append(productCard.elem);
+    });
+  }
+
+  updateFilter(filters) {
+    this.#initFilters();
+    this.elem.remove();
+    //this.elem = null;
+
+    let filteredProducts = this.products;
+    let speciesBefore = this.filters.maxSpiciness;
+
+    for (const [key, value] of Object.entries(filters)) {
+      this.filters[key] = value;
+    }
+
+    for (const [key, value] of Object.entries(this.filters)) {
+      if (key === "vegeterianOnly" && value === true) {
+        filteredProducts = this.#filterByVegeterian(filteredProducts);
+      }
+      if (key === "category" && value === 'soups') {
+        filteredProducts = this.#filterBySoups(filteredProducts);
+      }
+      if (key === "noNuts" && value === true) {
+        filteredProducts = this.#filterByNuts(filteredProducts);
+      }
+      if (key === "maxSpiciness" && value !== speciesBefore) {
+        filteredProducts = this.#filterBySpices(filteredProducts);
+      }
+    }
+
+    this.#filteredRender(filteredProducts);
+
+  }
+
+  #filteredRender(filteredProducts) {
+    this.elem = createElement(this.#mainTemplate());
+    this.#initElements();
+    filteredProducts.forEach((product) => {
+      let productCard = new ProductCard(product);
+      this.innerGrid.append(productCard.elem);
+    });
+    container.append(this.elem);
+  }
+
+  #filterBySoups(listToBeFiltered) {
+    return listToBeFiltered.filter(({category}) => category === 'soups');
+  }
+
+  #filterByNuts(listToBeFiltered) {
+    return listToBeFiltered.filter(({nuts}) => !nuts);
+  }
+
+  #filterBySpices(listToBeFiltered) {
+    return listToBeFiltered.filter(({spiciness}) => spiciness <= this.filters.maxSpiciness);
+  }
+
+  #filterByVegeterian(listToBeFiltered) {
+    let filtered = listToBeFiltered.filter(({vegeterian}) =>
+      vegeterian);
+    return filtered;
+  }
+
+  #initFilters() {
+    if (Object.keys(this.filters).length === 0) {
+      this.filters = {
+        noNuts: false,
+        vegeterianOnly: false,
+        maxSpiciness: 4,
+        category: '',
+      };
+      console.log('Filter initialized');
+    } else {
+      console.log('Filters already exists');
+    }
   }
 }
